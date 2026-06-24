d396e2dfad0f97137dea78cc72f0708db69f3c5d
lavan-mattu <lavan2245@gmail.com>
2026-06-24 11:01:21 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,166 @@
+#1st
+
+# a=10
+# b=20
+# if a>b:
+#     print("1st number is greatest")
+# else:
+#     print("2nd number is greatest")
+
+
+#2nd
+
+# a=int(input("Enter your number  "))
+# b=a%2
+# if b==0:
+#     print("It is an even number")
+# else:
+#     print("It is an odd number")
+
+
+#3rd
+
+# a=int(input("Enter your user id --> "))
+# if a>0:
+#     print("Your userID is valid ")
+# else:
+#     print("Your userID is not valid")
+
+
+#4th
+
+# a=10
+# b=15
+# c=20
+# if a>b and a>c:
+#     print("a is greatest")
+# elif b>a and b>c:
+#     print("b is greatest")
+# elif c>a and c>b:
+#     print("c is greatest")
+
+
+#5th
+
+# a=int(input("Enter your age --> "))
+# if a>=16:
+#     print("Age is valid to play the game")
+# else:
+#     print("Age is not valid to play the game")
+
+
+#6th
+
+# password=int(input("Enter your password -->  "))
+# conf_pass=int(input("Re-enter your password -->  "))
+# if password==conf_pass:
+#     print("Password matched")
+# else:
+#     print("Password not matched")
+
+
+#7th
+
+# cvv=int(input("Enter your card cvv number --> "))
+# if cvv>0:
+#     print("Transaction is under process")
+# else:
+#     print("Transaction declined")
+
+
+#8th incomplete
+
+# a=5
+# b=a-1
+# while b>0:
+#     print("answer is ",a*b)
+#     a-=1
+    
+
+#9th 
+
+# a=int(input("Enter your number  "))
+# b=a%2
+# if b==0:
+#     print("It is an even number")
+# else:
+#     print("It is an odd number")
+
+
+#10th
+
+# a=int(input("Enter the number --> "))
+# b=a%7
+# if b==0:
+#     print("The number is divisible by 7")
+# else:
+#     print("The number is not divisble by 7")
+    
+    
+#11th
+
+# a=int(input('Enter your number --> '))
+# b=a%7
+# c=a%4
+# if b==0 and c==0:
+#     print("The number is divisble by both 7 and 4")
+# else:
+#     print("The number is not divisible by both 7 and 4")
+
+
+#12th
+
+# a=int(input("Enter your number --> "))
+# b=a%5
+# if b==0:
+#     print("Hello Coder!")
+# else:
+#     print("Bye Loser....")
+
+
+#13th
+
+# a=int(input("Enter your number from 1 to 7 --> "))
+# if a==1:
+#     print("Sunday")
+# elif a==2:
+#     print("Monday")
+# elif a==3:
+#     print("Tuesday")
+# elif a==4:
+#     print("Wednesday")
+# elif a==5:
+#     print("Thursday")
+# elif a==6:
+#     print("Friday")
+# elif a==7:
+#     print("Saturday")
+
+
+#14th
+
+# a=input("Enter the city --> ")
+# if a=="delhi":
+#     print("Delhi : Red Fort")
+# else:
+#     print("Jaipur : Jal Mahal")
+
+
+#15th
+
+# cp=int(input("Enter the cost price --> "))
+# if cp>100000:
+#     tax=cp*0.15
+#     print("Cost Price      ", cp)
+#     print("Tax             ",tax)
+#     print("Final Price     ", cp+tax)
+# elif cp>50000 and cp<=100000:
+#     tax=cp*0.1
+#     print("Cost Price      ", cp)
+#     print("Tax             ",tax)
+#     print("Final Price     ", cp+tax)
+# elif cp<=50000 :
+#     tax=cp*0.05
+#     print("Cost Price      ", cp)
+#     print("Tax             ",tax)
+#     print("Final Price     ", cp+tax)
